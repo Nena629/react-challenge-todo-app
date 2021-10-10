937faa57d99d76f665f2d6dea5f2a0b2afdf72f8
Naima Attaf <naimattaf@gmail.com>
2021-10-10 07:31:43 +0200

    add notes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import React, { useState } from 'react'
 import AddNote from './components/AddNote'
 import ListeNote from './components/ListeNote'
 
-
 const App = () => {
    
     const [notes, setNotes] = useState([
-        {id: 1, content: "note 1"},{id: 2, content: "note 2"}, {id: 3, content: "note 3"}
+        {id: 1, content: "note 1"},
+        {id: 2, content: "note 2"}, 
+        {id: 3, content: "note 3"}
     ])
+    
+      //copieNotes
+    const [copieNotes, setCopieNotes] = useState([
+    { id: 1, content: "Text 1 😄" },
+    { id: 2, content: "Text 2 😄" },
+    { id: 3, content: "Text 3 😄" }
+    ])
+
+    const addNewNote = (newNote) => {
+
+        if (newNote !== "") {
+          setNotes([...notes, { id: Math.random() * 100, content: newNote }])
+          setCopieNotes([...notes, newNote])
+        }
+    
+      }
     
     return (
         <div style={{ textAlign: "center" }}>
             <h1>Note app</h1>
         {/* listeNote*/}
-            <AddNote/>
+            <AddNote addListNote= {addNewNote} />
             <hr />
             <div className="filter border w-50 mx-auto">
                 <input type="text" placeholder="filter task by title" className="form-control" />
--- src/components/addNote.js
+++ src/components/addNote.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import { useRef } from 'react'
 
-const AddNote = () => {
-    return (
-        <div>
-            <div style={{ margin: "30px", width: "1200px", justifyContent: "center" }}>
-                <input type="text" placeholder="Add a Note" className="form-control" />
-                <button className="btn btn-success">+</button>
-            </div>
-        </div>
-    )
+
+const AddNote = ({addListNote}) => {
+
+  const inputText = useRef();
+
+  const onAddNote = () => {
+    // alert(inputText.current.value)
+    if (window.confirm("are you sure ?") === true) {
+     addListNote(inputText.current.value);
+    }
+  
+  }
+
+  return (
+
+    <div style={{ margin: "30px", width: "1200px", justifyContent: "center" }}>
+      <div className="add">
+        <input
+          type="text" className="form-       control" placeholder=" write the text" ref={inputText}
+        />
+
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => onAddNote()}> Add </button>
+      </div>
+    </div>
+  )
 }
+
 
 export default AddNote
