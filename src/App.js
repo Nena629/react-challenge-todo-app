bff48601e9a5b4e0e1d957a5ba09be7314498745
Naima Attaf <naimattaf@gmail.com>
2021-12-27 23:03:35 +0100

    express app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react'
-import FuncComp from './Components/FuncComp'
+const express= require('express')
+const app= express()
+const port= 3000
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
-function App() {
-  const [show, setShow] = useState(true)
-  
-  return (
-    <div>
-      {show === true ? <FuncComp/> : null}
-      <button onClick={()=>setShow(!show)}> 
-      {show === true ?'delete':'creer'} component</button>
-    </div>
-    
-  )
+function middlewareActivated(req, res, next) {
+    const date = new Date()
+    const day = date.getDay()
+    const hour = date.getHours()
+    day === 0 || day === 6 ? res.end('application hors service') :
+    (hour < 9 || hour > 17 ? res.end('application hors serevice') : '')
+    next()
 }
 
-export default App
+app.get('/', middlewareActivated, (req,res) => {
+    res.render('Home',{
+        title:'the title',
+        message:'Hello'
+    })
+})
+app.get('/Home', (req,res) => {
+    res.render('Home',{
+        title:'the title',
+        message:'This is the home page'
+    })
+})
+app.get('/Services', (req,res) => {
+    res.render('Services',{
+        title:'the title',
+        message:'Our services'
+    })
+})
+app.get('/Contact', (req,res) => {
+    res.render('Contact',{
+        title:'the title',
+        message:'Contact us'
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`server listening at http://localhost:${port}`)
+})
